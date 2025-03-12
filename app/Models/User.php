a32f8164741a2ff66c6debf8237a0b6411c9d170
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-12 09:13:12 +0000

    Update to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +19,40 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'title',
+        'first_name',
+        'middle_name',
+        'last_name',
         'email',
         'password',
+        'profile_picture',
+        'cv_path',
+        'cover_letter_path',
+        'phone',
+        'salary',
+        'first_line',
+        'second_line',
+        'town',
+        'city',
+        'county',
+        'country',
+        'post_code',
+        'full_or_part',
+        'region',
+        'timezone',
+        'start_date',
+        'end_date',
+        'is_live',
+        'role',
+        'seniority',
+        'department_id',
+        'job_id',
+        'holiday_entitlement_id',
+        'contact_id',
+        'contract_id',
+        'created_by',
+        'updated_by',
+        'deleted_by',
     ];
 
     /**
@@ -43,6 +75,11 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'start_date' => 'date',
+            'end_date' => 'date',
+            'is_live' => 'boolean',
+            'role' => 'string',
+            'seniority' => 'string',
         ];
     }
 }
