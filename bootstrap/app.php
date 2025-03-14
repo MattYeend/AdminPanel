b5944c3d686fc8bafe3d6b843099dae3672423e2
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-14 10:33:41 +0000

    #3 New tenant route and update to bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Spatie\Multitenancy\Http\Middleware\NeedsTenant;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function ($router) {
+            require base_path('routes/tenant.php'); 
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance']);
@@ -20,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            NeedsTenant::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
