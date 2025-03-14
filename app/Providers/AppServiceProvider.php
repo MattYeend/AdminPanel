d79aed5de5d4605ba2b9e50f15ffc7eda5918243
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-14 14:58:07 +0000

    #3 Update to appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (!function_exists('tenant')) {
+            function tenant() {
+                return app('tenant');
+            }
+        }
     }
 }
